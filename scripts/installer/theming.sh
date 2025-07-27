df4fe001cb91adb62dc579becd8ac29440b3e2ff
lapointek <156107790+lapointek@users.noreply.github.com>
2025-07-27 10:57:22 -0400

    modified prerequisites, theming, utilities install scripts

--- scripts/installer/theming.sh
+++ scripts/installer/theming.sh
@@ -17,7 +17,7 @@ run_command "tar -xvf /home/$SUDO_USER/simple-hyprland/assets/themes/Catppuccin-
 
 run_command "tar -xvf /home/$SUDO_USER/simple-hyprland/assets/icons/Tela-circle-dracula.tar.xz -C /usr/share/icons/" "Install Tela Circle Dracula icon theme" "yes"
 
-run_command "yay -S --sudoloop --noconfirm kvantum-theme-catppuccin-git" "Install Catppuccin theme for Kvantum" "yes" "no"
+run_command "paru -S --sudoloop --noconfirm kvantum-theme-catppuccin-git" "Install Catppuccin theme for Kvantum" "yes" "no"
 
 run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/kitty /home/$SUDO_USER/.config/" "Copy Catppuccin theme configuration for Kitty terminal" "yes" "no"
 
